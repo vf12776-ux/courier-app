49e0a0bd6f6ca21129d2912d102775d4963cac5e
vf12776-ux <vf12776@gmail.com>
2025-11-14 02:59:31 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Приложение Курьера',
-        short_name: 'Курьер',
-        description: 'Приложение для курьеров доставки',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/courier-app/',
-        scope: '/courier-app/',
-        icons: [
-          {
-            src: 'icon55.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon55.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   base: '/courier-app/',
 })
