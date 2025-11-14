1efb954450c40630f9edbfd42b241543959c2602
vf12776-ux <vf12776@gmail.com>
2025-11-14 03:29:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: '/courier-app/',
-})
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
+});
